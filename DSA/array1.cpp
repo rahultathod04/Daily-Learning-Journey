f40bfed7e9edcb4195e53187aeeeb6ea567916b6
Rahul Tathod <rahultathod04@gmail.com>
2026-08-06 16:46:38 +0530

    Day03_lists_and_tuples

---
+++ DSA/array1.cpp
@@ -0,0 +1,14 @@
+// array is a collection a similar(data-type) items
+
+
+
+
+n = len(num)
+count = 1
+flg= True
+for i in range (0, n-1):
+        if(num[i]>num[i+1]):
+            count+=1
+if(count==0 or count== 1)
+     flg = true
+else : flg = false
