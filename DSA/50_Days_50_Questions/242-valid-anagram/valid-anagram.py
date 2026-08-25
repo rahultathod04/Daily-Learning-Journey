541f7b893d7202441eb34c9831118f8969a7ca20
Rahul Tathod <rahultathod04@gmail.com>
2026-08-25 21:14:26 +0530

    Time: 21 ms (53.02%) | Memory: 14.2 MB (12.92%) - LeetSync

---
+++ DSA/50_Days_50_Questions/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+         return False
+        count = [0] * 26
+        for i in range(len(s)):
+         count[ord(s[i]) - ord('a')] += 1
+         count[ord(t[i]) - ord('a')] -= 1
+        return all(x == 0 for x in count)
