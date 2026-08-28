4878f8fbb6d56ca7f60c6fded7f1b7efdb53074c
Rahul Tathod <rahultathod04@gmail.com>
2026-08-28 21:39:15 +0530

    Time: 13 ms (97.87%) | Memory: 16.2 MB (53.87%) - LeetSync

---
+++ DSA/50_Days_50_Questions/49-group-anagrams/group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            anagram_map[sorted_s].append(s)
+        return list(anagram_map.values())
+        
