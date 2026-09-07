5ab927968094fb286376469d4d4296129dc6d4fc
Rahul Tathod <rahultathod04@gmail.com>
2026-09-07 17:04:56 +0530

    Time: 11 ms (50.49%) | Memory: 14.6 MB (6.58%) - LeetSync

--- DSA/50_Days_50_Questions/169-majority-element/majority-element.py
+++ DSA/50_Days_50_Questions/169-majority-element/majority-element.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def majorityElement(self, nums):
         n = len(nums)
-        
-        frq = 0
         ans = nums[0]
+        frq = 0 
 
         for i in range(n):
-         if(frq==0):
-             ans = nums[i]
-         if(ans==nums[i]):
-             frq +=1
-         else:
-             frq-=1
-
+            if(frq == 0):
+                ans = nums[i]
+            if(ans == nums[i]):
+                frq+=1
+            else:
+                frq-=1
         return ans
