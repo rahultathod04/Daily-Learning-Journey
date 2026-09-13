ff8eacae3ebaef5e441d743984e7c91b23f0fa88
Rahul Tathod <rahultathod04@gmail.com>
2026-09-13 11:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.05%) - LeetSync

--- DSA/50_Days_50_Questions/75-sort-colors/sort-colors.py
+++ DSA/50_Days_50_Questions/75-sort-colors/sort-colors.py
@@ -1,19 +1,23 @@
 class Solution:
     def sortColors(self, nums):
+        n = len(nums)
         low = 0
         mid = 0
-        high = len(nums) - 1
+        high = n-1 
 
-        while mid <= high:
+        while (mid <= high):
 
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
+            if(nums[mid] == 0):
+                nums[mid] = nums[low]
+                nums[low] = 0
+                low+=1
+                mid+=1
 
-            elif nums[mid] == 1:
-                mid += 1
+            elif(nums[mid] == 1):
+                mid+=1
 
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+                nums[mid] = nums[high]
+                nums[high] = 2
+                high -=1
+   
