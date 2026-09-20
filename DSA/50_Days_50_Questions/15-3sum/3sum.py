598839089c5bc25922bb2e7b7af458ad2cbd6f42
Rahul Tathod <rahultathod04@gmail.com>
2026-09-20 19:06:47 +0530

    Time: 669 ms (81.48%) | Memory: 18.4 MB (60.31%) - LeetSync

--- DSA/50_Days_50_Questions/15-3sum/3sum.py
+++ DSA/50_Days_50_Questions/15-3sum/3sum.py
@@ -1,40 +1,36 @@
 class Solution(object): 
     def threeSum(self, nums): 
-        n = len(nums) 
-        nums.sort() 
-        result = [] 
- 
-        for i in range(n - 2): 
+        n = len(nums)
+        nums.sort()
+        result = []
 
-            if nums[i] > 0: 
-                break 
+        for i in range(n-2):
+            if(nums[i]>0):
+             break
+            if(i>0 and nums[i] == nums[i-1]):
+                continue
+            
+            left = i+1
+            right = n-1
 
-            if i > 0 and nums[i] == nums[i - 1]: 
-                continue 
+            while(left < right):
+                sum = nums[i] + nums[left] + nums[right]
 
-            left = i + 1 
-            right = n - 1 
+                if(sum == 0):
+                    l = [nums[i],nums[left],nums[right]]
+                    result.append(l)
+                    left+=1
+                    right-=1
 
-            while left < right:
+                    while(left<right and nums[left]==nums[left-1]):
+                        left+=1
+                    while(left<right and nums[right]==nums[right+1]):
+                        right-=1
+                    
+                elif(sum<0):
+                    left+=1
 
-                sum = nums[i] + nums[left] + nums[right] 
- 
-                if sum == 0: 
-                    result.append([nums[i], nums[left], nums[right]]) 
+                else:
+                    right-=1
 
-                    left += 1 
-                    right -= 1 
-
-                    while left < right and nums[left] == nums[left - 1]: 
-                        left += 1 
-
-                    while left < right and nums[right] == nums[right + 1]: 
-                        right -= 1 
-
-                elif sum < 0: 
-                    left += 1 
-
-                else: 
-                    right -= 1 
- 
-        return result
+        return result 
