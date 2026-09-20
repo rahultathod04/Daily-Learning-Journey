081d6ea4ad03da1f2e8168d82597308c845d616a
Rahul Tathod <rahultathod04@gmail.com>
2026-09-20 19:31:24 +0530

    Time: 129 ms (10.00%) | Memory: 19.9 MB (98.97%) - LeetSync

--- DSA/50_Days_50_Questions/11-container-with-most-water/container-with-most-water.py
+++ DSA/50_Days_50_Questions/11-container-with-most-water/container-with-most-water.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxArea(self, height):
-        n = len(height)
-        l = 0
-        r = n-1
-        max_water = 0
+       n = len(height)
+       l = 0
+       r = n-1
+       ans = 0
 
-        while l<r:
-            h = min(height[l], height[r])
-            w = r-l
-            area = w * h
-            max_water = max(max_water, area)
+       for i in range(n):
+        w = r-l
+        h = min(height[l], height[r])
+        area = w*h
+        ans = max(area, ans)
 
-            if(height[l]<height[r]):
-                l+=1
-            else:
-                r-=1
-
-        return max_water
+        if(height[l]<height[r]):
+            l+=1
+        else:
+            r-=1
+       return ans
